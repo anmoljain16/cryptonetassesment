5338b8376e9798f57a8ad77b068e99cc8d5557f6
anmoljain16 <ajanmoldosi@gmail.com>
2024-04-29 10:46:08 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {useEffect, useState} from "react";
+import Card from "./components/Card";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+async function getUsers(){
+    try{
+        const response = await fetch('https://randomuser.me/api/?page=1&results=1&seed=abc').then(response => response.json());
+
+        return response;
+    }catch (e) {
+        return {
+            error: true,
+            message: e.message
+        }
+    }
+
 }
 
-export default App;
+export default function App() {
+    const[users, setUsers] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(()=>{
+        setLoading(true)
+        getUsers().then(response => {
+
+            // console.log(response);
+            if(!response.error){
+                setUsers(response.results[0]);
+            }
+
+        }).finally(() => {
+            setLoading(false)
+        }
+        )
+
+        console.log(users)
+    },[])
+
+    if(loading){
+        return <h1>Loading...</h1>
+    }
+
+    if(!loading && !users){
+        return <h1>Something went wrong</h1>
+    }
+
+  return (
+      <>
+            <Card user={users}/>
+      </>
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+export default function Card({user}) {
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <div className="bg-gray-100 p-4 border-2   w-fit shadow-2xl shadow-black/70 scale-125">
+                <div className="flex items-center space-x-4">
+                    <img src={user.picture.large} alt="user" className="w-24 h-24 m-5 border-2 "/>
+                    <div className={"m-5"}>
+                        <h1 className="text-xl font-bold">{user.name.first} {user.name.last}</h1>
+                        <p className="text-base text-gray-500"> {(user.gender).slice(0,1).toUpperCase() + (user.gender).slice(1)}</p>
+                        <p className="text-base text-gray-500"> {user.phone}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
